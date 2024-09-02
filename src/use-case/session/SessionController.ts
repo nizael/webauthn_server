1a6a903a852465b52ccdf691b26c2ac5101e44b5
nizael <nizael@nizael.com.br>
2024-09-02 01:51:56 -0300

    chore: fix generateAuthentication

--- src/use-case/session/SessionController.ts
+++ src/use-case/session/SessionController.ts
@@ -1,12 +1,12 @@
-import { Request, Response } from 'express';
+import { Request, Response } from 'express'
 import bcrypt from 'bcrypt'
-import crypto from 'crypto';
+import crypto from 'crypto'
 
 let users: {
-  id: string
+  id: string,
   username: string
   password: string
-}[] = [];
+}[] = []
 
 let credentials: {
   credentialId: string
@@ -17,55 +17,49 @@ let credentials: {
 
 export class SessionController {
   async generateRegistration(req: Request, res: Response) {
-    const { username, password } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
+    const { username, password } = req.body
+    const hashedPassword = await bcrypt.hash(password, 10)
 
     const id = crypto.randomUUID()
+
     users.push({
       id,
       username,
       password: hashedPassword,
     })
 
-    const challenge = crypto.randomBytes(32).toString('base64url');
+    const challenge = crypto.randomBytes(32).toString('base64url')
 
-    res.json({ challenge, userId: id });
+    res.json({ challenge, userId: id })
   }
 
   async verifyRegistration(req: Request, res: Response) {
-    const { userId, credentialId, publicKey, signCount } = req.body;
+    const { userId, credentialId, publicKey, signCount } = req.body
     credentials.push({
       credentialId,
       publicKey,
       signCount,
       userId,
     })
-    res.status(201).send('Registration complete');
+    res.status(201).send('Registration complete')
   }
 
   async generateAuthentication(req: Request, res: Response) {
-    const { username } = req.body;
-
-    const user = users.find(user => user.username === username);
-    if (!user) {
-      return res.status(400).send('User not found');
-    }
-
-    const webAuthnCreds = credentials.filter(cred => cred.userId === user.id);
-
-    const challenge = crypto.randomBytes(32).toString('base64url');
-
-    res.json({ challenge, credentials: webAuthnCreds });
+    const challenge = crypto.randomBytes(32).toString('base64url')
+    res.json({
+      challenge,
+    })
   }
 
   async verifyAuthentication(req: Request, res: Response) {
-    const { credentialId } = req.body;
+    const { credentialId } = req.body
 
-    const credential = credentials.find(cred => cred.credentialId === credentialId);
+    const credential = credentials.find(cred => cred.credentialId === credentialId)
 
     if (!credential) {
-      return res.status(400).send('Credential not found');
+      return res.status(400).send('Credential not found')
     }
-    res.status(200).send('Authentication successful');
+
+    res.status(200).send('Authentication successful')
   }
 }
